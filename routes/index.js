8863190dd35f62ca3d26f492a5f2f221b2b8c365
Jhanira-Elizabeth <jearanda@espe.edu.ec>
2025-05-15 14:22:45 -0500

    Correcion index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const puntosRouter = require('./puntos_turisticos.router');
 const etiquetaRouter = require('./etiquetas_turisticas.router');
 const actividadRouter = require('./actividad.router');
 const localEtiquetaRouter = require('./local_etiqueta.router');
+const puntoEtiquetaRouter = require('./punto_etiqueta.router');
 const serviciosRouter = require('./servicios_locales.router');
 const duenosRouter = require('./duenos_locales.router');
 const parroquiasRouter = require('./parroquias.router');
 const horariosRouter = require('./horarios_atencion.router');
-const puntoEtiquetaRouter = require('./puntos_etiquetas.router');
 
 function routerApi(app) {
   const router = express.Router();
@@ -19,11 +19,11 @@ function routerApi(app) {
   router.use('/etiquetas', etiquetaRouter);
   router.use('/actividades', actividadRouter);
   router.use('/local-etiqueta', localEtiquetaRouter);
+  router.use('/punto-etiqueta', puntoEtiquetaRouter); 
   router.use('/servicios', serviciosRouter);
   router.use('/duenos', duenosRouter);
   router.use('/parroquias', parroquiasRouter);
   router.use('/horarios', horariosRouter);
-  router.use('/punto-etiqueta', puntoEtiquetaRouter);
 }
 
 module.exports = routerApi;
